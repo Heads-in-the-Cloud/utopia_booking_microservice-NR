5b4270810aa2abb3a6b6104d6582e53c32d8a811
nick-sstack <nicholas.ruiz@smoothstack.com>
2021-11-04 11:48:56 -0700

    Deleted unnecessary entities

--- src/main/java/com/smoothstack/utopia/booking/repository/FlightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smoothstack.utopia.booking.repository;
-
-import com.smoothstack.utopia.booking.entity.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight, Integer> {
-}
