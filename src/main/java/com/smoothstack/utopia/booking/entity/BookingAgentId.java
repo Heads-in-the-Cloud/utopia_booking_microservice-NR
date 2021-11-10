f8441ef49590fbbe053c86b6e5f8e7af41174057
nick-sstack <nicholas.ruiz@smoothstack.com>
2021-11-10 13:13:44 -0800

    Changed from Java 17 to Java 8

--- src/main/java/com/smoothstack/utopia/booking/entity/BookingAgentId.java
+++ src/main/java/com/smoothstack/utopia/booking/entity/BookingAgentId.java
@@ -3,14 +3,12 @@ package com.smoothstack.utopia.booking.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 @NoArgsConstructor
 @Data
 public class BookingAgentId implements Serializable {
 
-    @Serial
     private static final long serialVersionUID = 1L;
 
     private int agentId;
